860268cc64e672a35cb3470c222bcbf6e88ed770
ShellyKelly1 <128622217+ShellyKelly1@users.noreply.github.com>
2024-12-11 13:24:44 +0300

    Update test_calculator.py

--- calculator/tests/test_calculator.py
+++ calculator/tests/test_calculator.py
@@ -3,16 +3,16 @@ from src.calculator import add, subtract, divide, multiply
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 6), 8)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(2, 1), 1)
 
     def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(6, 4), 24)
 
     def test_divide(self):
-        self.assertEqual(divide(6, 2), 3)
+        self.assertEqual(divide(8, 4), 2)
         with self.assertRaises(ValueError):
             divide(1, 0)
 
