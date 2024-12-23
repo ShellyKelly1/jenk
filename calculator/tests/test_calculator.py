d99ca903ac6175e180ffba6e464b0b3c43e11be2
ShellyKelly1 <128622217+ShellyKelly1@users.noreply.github.com>
2024-12-23 15:42:30 +0300

    Update test_calculator.py

--- calculator/tests/test_calculator.py
+++ calculator/tests/test_calculator.py
@@ -5,12 +5,6 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 6), 8)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(2, 1), 1)
-
-    def test_multiply(self):
-        self.assertEqual(multiply(6, 4), 24)
-
 
 
 if __name__ == '__main__':
